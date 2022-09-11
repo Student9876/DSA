38686382d56b661fcccbb1a882a8508dfadee39f
student9876 <shouvik9876@gmail.com>
2022-09-11 11:03:00 +0530

    Edited display function in Queue using array in C++

--- Code/Queue_using_array.cpp
+++ Code/Queue_using_array.cpp
@@ -1,60 +1,70 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-template<class T>
-class queue{
-    public:
-        int size;
-        int f = -1;
-        int r = -1;
-        T *arr;
-    int isEmpty(){
-        if(f ==r){
+template <class T>
+class queue
+{
+public:
+    int size;
+    int f = -1;
+    int r = -1;
+    int count = 0;
+    T *arr;
+    int isEmpty()
+    {
+        if (f == r)
+        {
             return 1;
         }
         return 0;
     }
-    int isFull(){
-        if(r == size-1){
+    int isFull()
+    {
+        if (r == size - 1)
+        {
             return 1;
         }
         return 0;
     }
-    void enqueue(T val){
-        if(isFull()){
-            cout<<"Queue is Full"<<endl;
+    void enqueue(T val)
+    {
+        if (isFull())
+        {
+            cout << "Queue is Full" << endl;
         }
         r++;
         arr[r] = val;
     }
-    T dequeue(){
+    T dequeue()
+    {
         int a = -1;
-        if(isEmpty()){
-            cout<<"Queue is Empty"<<endl;
+        if (isEmpty())
+        {
+            cout << "Queue is Empty" << endl;
         }
         f++;
+        count++;
         a = arr[f];
         return a;
     }
-    void display(){
-        for (int i = f; i <= r; i++)
+    void display()
+    {
+        for (int i = count; i <= r; i++)
         {
-            cout<<arr[i]<<"\t";
+            cout << arr[i] << "\t";
         }
-        cout<<endl;
+        cout << endl;
     }
 };
 
-
 int main()
 {
-    queue <int>q;
+    queue<int> q;
     q.size = 10;
     // q.f = q.r = -1;
     q.arr = new int(q.size);
 
-     // Enqueue few elements
+    // Enqueue few elements
     q.enqueue(12);
     q.enqueue(43);
     q.enqueue(36);
@@ -62,9 +72,9 @@ int main()
 
     q.display();
 
-    //Dequeuing element
-    cout<<"Dequeued Element: "<<q.dequeue()<<endl;
-    cout<<"Dequeued Element: "<<q.dequeue()<<endl;
+    // Dequeuing element
+    cout << "Dequeued Element: " << q.dequeue() << endl;
+    cout << "Dequeued Element: " << q.dequeue() << endl;
 
     q.display();
     return 0;
